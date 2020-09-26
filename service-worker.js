629e4fdf47869d4e32ebe0fdb0b7093030cc3480
Salman Shaikh <tony903212@gmail.com>
2020-09-26 19:04:26 +0530

    Converting to PWA Added Icons, Added manifest, and added Service Worker

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const cacheName = "Calculator";
+const filesToCache = [
+  "/",
+  "index.html",
+  "main.js",
+  "main.css"
+];
+
+self.addEventListener("install", e => {
+  console.log("[ServiceWorker**] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      console.log("[ServiceWorker**] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", event => {
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] - Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    });
+  });
+
+  self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request, { ignoreSearch: true }).then(response => {
+        return response || fetch(event.request);
+      })
+    );
+  });  
+  
